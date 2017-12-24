a7ecbcc357e3e5c6830092e63c27d02e5dee44ae
Terrance Williams <terrance.d.williams1976@gmail.com>
2017-12-24 03:11:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board (board)
+
+puts " |   |
+    -----------
+       |   |
+    -----------
+       |   |   "
+end
+
+board = [" "," "," "," ","X"," "," "," "," "]
+def display_board (board)
+
+puts " |   |
+    -----------
+       | X |
+    -----------
+       |   |   "
+end
